ec45e0619868faaf37853497124146d875fbc4dd
ksouri <ksouriksour5@gmail.com>
2024-12-16 13:27:38 +0100

    html

---
+++ Programing Basics/week1/day4/practice/LoopDLoop.js
@@ -0,0 +1,10 @@
+// i know that i have to use loop here because i have an act that many time
+// strting of the loop is 2
+// the loop shoud steop at 6
+// the lopwill know to  stop when i<=6
+// 
+// varaibels 
+
+for(i=2;i<6;i+=2){
+    console.log(i)
+}
