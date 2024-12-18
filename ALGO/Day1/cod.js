42e230bff987b41ebbc815e3667082220b48b985
ksouri <ksouriksour5@gmail.com>
2024-12-18 15:36:30 +0100

    html

---
+++ ALGO/Day1/cod.js
@@ -0,0 +1,4 @@
+// o see if you can  afford to buy thingi need to creat a foction that need tow things 
+// (THE BUDJET  and the Cost)
+// In the case where the budjet=>the cost of the thing  the return gonna be yes you can afford to buy this thing
+// and in the case th budjet<the cost of the thning the return gonna be noyou can not afford to buy this thing
