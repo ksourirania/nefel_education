7a56ea3f035a4867159f875d3bc948c71ebe654f
ksouri <ksouriksour5@gmail.com>
2024-12-23 18:13:41 +0100

    html

---
+++ WEB FUNDAMENTALS/week2/day1/practice/Loop Challenges/loop.js
@@ -0,0 +1,29 @@
+// chalange1:
+for(i=1; i<= 20; i++){
+    if(i %2 !== 0) {
+        console.log(i);
+    }
+}
+// chalange 2:
+for(var i = 100; i>=0; i--){
+    if(i%3 !== 0){
+        console.log(i)
+    }
+}
+// chalang3:
+for( var i = 4; i>= -3.5; i-=1.5){
+    console.log(i);
+}
+// challange4:
+var sum =0;
+for (var i=1; i<=100 ; i++){
+      sum +=i
+}
+
+console.log(sum)
+// chalange5:
+var porodect=1
+for(var i =1; i<=12 ; i++){
+    porodect*=i;
+}
+console.log(porodect)
