397992fa96428f21d17762dc08ce3750606d156d
ksouri <ksouriksour5@gmail.com>
2024-12-25 17:22:36 +0100

    html

---
+++ WEB FUNDAMENTALS/week2/day2/practice/Video Preview/video-preview/script.js
@@ -0,0 +1,9 @@
+console.log("page loaded...");
+function playvideo(vid){
+    console.log(vid);
+    Vid.play();
+}
+fuction pausevideo(vid){
+    console.log(vid):
+    Vid.play();
+}
---
+++ WEB FUNDAMENTALS/week2/day3/practice/DojoWeather/script.js
@@ -0,0 +1,10 @@
+function loding(){
+    alert("loding weather report...")
+}
+var cookie=document.querySelector(".cookies")
+function accept(){
+    cookie.remove();
+}
+fuction change(e){
+    console.log(e.value);
+}
